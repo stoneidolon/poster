70e89471f5a1cde8dff8230e643d7672dd62bb2e
Chris Su <zerogpm@gmail.com>
2016-10-02 18:25:32 -0400

    add well side menu

--- resources/views/partials/_message.blade.php
+++ resources/views/partials/_message.blade.php
@@ -3,3 +3,18 @@
         <strong>Success:</strong> {{ Session::get('success') }}
     </div>
 @endif
+
+@if (count($errors) > 0)
+
+    <div class="alert alert-danger" role="alert">
+        <strong>Errors:</strong>
+        <ul>
+            @foreach($errors->all() as $error)
+                <li>
+                    {{ $error }}
+                </li>
+            @endforeach
+        </ul>
+    </div>
+
+@endif
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -11,5 +11,35 @@
 @stop
 
 @section('content')
-    <p class="lead">This is the blog post</p>
+    <div class="row">
+        <div class="col-md-8">
+            <h1>{{ $post->title }}</h1>
+
+            <p class="lead">{{ $post->body }}</p>
+        </div>
+
+        <div class="col-md-4">
+            <div class="well">
+                <div class="dl-horizontal">
+                    <dt>Create At:</dt>
+                    <dd>{{ $post->created_at->format('M j, Y h:ia') }}</dd>
+                </div>
+
+                <div class="dl-horizontal">
+                    <dt>Last Updated:</dt>
+                    <dd>{{ $post->updated_at->format('M j, Y h:ia') }}</dd>
+                </div>
+
+                <hr>
+                <div class="row">
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.edit', 'Edit', [$post->id], ['class' => 'btn btn-primary btn-block']) !!}
+                    </div>
+                    <div class="col-sm-6">
+                        {!! Html::linkRoute('posts.destroy', 'Delete', [$post->id], ['class' => 'btn btn-danger btn-block']) !!}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
